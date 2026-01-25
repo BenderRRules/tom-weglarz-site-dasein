f86e630244671fc0fcd17cd2652a7cd53f468d69
BenderRRules <tom.weglarz.01@gmail.com>
2026-01-25 15:02:30 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,28 @@
 // @ts-check
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 import icon from "astro-icon";
 import cloudflare from "@astrojs/cloudflare";
 import { defineConfig, fontProviders } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tomweglarz.com",
-
   output: "server",
-  adapter: cloudflare(),
-
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
   integrations: [mdx(), sitemap(), pagefind(), icon()],
-
   vite: {
     plugins: [tailwindcss()],
+    ssr: {
+      external: ["node:path", "node:path/posix"]
+    }
   },
-
   experimental: {
     fonts: [
       {
